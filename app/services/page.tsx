d376c503d46734ec59a0ea00fc3af49b7a117c60
thaquidheen <thaquidheen@gmail.com>
2026-01-08 10:13:59 +0530

    lip

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -21,7 +21,7 @@ export default function ServicesPage() {
       />
       <ServiceShowcase />
       <FreeZoneSlider />
-      <PricingPackages />
+      {/* <PricingPackages /> */}
       <FAQ />
       <CTABanner />
     </>
